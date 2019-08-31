272cec2ea7a416f9811a923557f12facf558f0e3
Eduardo Francisco <eadwardus@disroot.org>
2019-08-31 15:12:22 -0300

    Adapt to new API

--- src/stdlib/bsearch.c
+++ src/stdlib/bsearch.c
@@ -7,6 +7,6 @@ void *
 bsearch(const void *k, const void *b, size_t n, size_t m,
     int (*f)(const void *, const void *))
 {
-	return c_std_bsearch((void *)k, (void *)b, n, m,
+	return c_std_bsearch((void *)k, m, n, (void *)b,
 	    (int (*)(void *, void *))f);
 }
